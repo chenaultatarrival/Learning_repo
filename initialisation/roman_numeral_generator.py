514041e7f2fe811501188c2868ba163e2e311f75
chenault <chenault@arrival.com>
2022-10-10 16:20:20 +0100

    parameterised tests working

--- initialisation/roman_numeral_generator.py
+++ initialisation/roman_numeral_generator.py
@@ -1,20 +1,16 @@
-class RomanNumeralGenerator(object):
- 
+class RomanNumeralGenerator():
     def generator(self, arabic):
         roman = self.to_roman(arabic)
         return roman
- 
+
     def to_roman(self, number):
         roman_number = ""
-        if (number == 5):
-            roman_number = 'V'
-        elif (number == 4):
-            roman_number = 'IV'
+        if number == 5:
+            roman_number = "V"
+        elif number == 4:
+            roman_number = "IV"
         else:
-            while (number > 0):
+            while number > 0:
                 roman_number += "I"
                 number -= 1
         return roman_number
-        
-        
-  
