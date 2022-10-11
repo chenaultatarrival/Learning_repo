f3de74a0a180be9caac2948efd87c6a4a0222763
chenault <chenault@arrival.com>
2022-10-11 15:57:29 +0100

    working

--- initialisation/roman_numeral_generator.py
+++ initialisation/roman_numeral_generator.py
@@ -3,14 +3,15 @@ class RomanNumeralGenerator():
         roman = self.to_roman(arabic)
         return roman
 
-    def to_roman(self, number):
-        roman_number = ""
-        if number == 5:
-            roman_number = "V"
-        elif number == 4:
-            roman_number = "IV"
-        else:
-            while number > 0:
-                roman_number += "I"
-                number -= 1
-        return roman_number
+    def to_roman(self, x):
+        numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+        roman = ['I','IV','V','IX', 'X','XL', 'L','XC', 'C','CD', 'D','CM','M']
+        i = 12  
+        roman_numeral = ''
+        while x != 0:
+            if numbers[i] <= x:    
+                roman_numeral += roman[i] 
+                x = x - numbers[i]
+            else:
+                i -= 1
+        return roman_numeral 
